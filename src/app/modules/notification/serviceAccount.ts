d08da586193a69d6d270f0b08d7fa3d83e3cf7a9
rajon38 <rashedul.rajon@gmail.com>
2025-08-16 12:28:59 +0600

    refactor: streamline Firebase Admin SDK initialization and add service account configuration

---
+++ src/app/modules/notification/serviceAccount.ts
@@ -0,0 +1,18 @@
+export const serviceAccount = {
+  type: "service_account",
+  project_id: "evriman-eaba7",
+  private_key_id: "d0f09dbe9d8cda831c20a5378f6a5086795fb74e",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC/hAGfzKKp+Ekk\ns/IIi81XX2E76vmK/+vvJFP9iRgq8RKiXTFOktw3jOfACN1cihPU/2zN9bc1YYZa\n/roXatIsPRUS3QmyRx06ETuCOquH56kJ2eZ6s2maeUZdrV///1HEbcGHWDg1OC2F\nVl0Sy0fbK0o+hIFC2uO/hhZNfOpz1FD9uIA4Dz7bd9uCC/h3p7PRa+FoqXYJNC4n\nkI55XEE/eAUgNaDlfrt8UGmj4oCpiKNddb55T0W+qvwUXrMoltzLEaNz7qCipGa7\nr9cT7twrZhc+GgJ5UbMwioxSWbHaJGoJVwDE0LjoPq7LoxSsfkOScZe4gjD6Vh5P\nmeV1n5TFAgMBAAECggEAIYCNqi8pi3NAFFgYMG+IWc75Gvq1cZ/oXXqs6K65WBW4\n7UdBL14YgO04w0qpsp0C8CFIntTrmj53uuZRlnQc1RlQXO7m2dV/4t8N2m+5/vnD\nA9Km83L8N8YcTZndL9KxspuCndMMbh7oM6mSeY2vAcOWZs5WMGRLL/9cWmzONLmz\nSUhG4B5Weqd07Z1r30Nu6WP/BYSEatDcuoKyc+xyBIZC7wdzuAeYxXDKdPTHuTWQ\ncPi9tQueXXHjvea3pydx2vpUCbJnMuVwIiQPfI0bkHmLEdxeCt0+KqEGQsej8HWc\nvRsviqr/8PRw/9UFs7hr8t/2tguZS0/UnmsAJGS/4QKBgQDvBUq1pZKs2O54Kyf/\nEEbdWI/tDtu1iPrMF19m+Ot/ZmuyXAvCwfwHDr58/fy1Z+ZdP7QiRX8ednTte+o3\ndxzud7jsDlF4Ye0LyOd22VDbgVKPIBtlbmJ3QvVPPHH76SoN+LTXzPO9KctgC3Lk\nEXx7F0kHZ9mo/zUsoTakIjJHMQKBgQDNHs/4zvNw3ItAMNJSpAfFfoWYkFfoa0t9\n1Ux/ensNdQyPByTzG1eLtXLBGXL1flHiHIYqC8pzPyO7MsnQt4Oqom9CCscPul2t\nmbGTBtrIH5tXLv0s4tidAn2VKJ6kiHN1/CigdU+k+4ZpauYqcr5awcohC6QiwxEi\n2Isw0Lup1QKBgDn1uowZvluTryAMtwIu3EBIfX4qoDSBJu/eqw+5RMH8ABmcIG31\nxDuh46dDn8gNjdsG25OD7rNgXjJIXh9dDvL0rS7wuejLNkQpT5eOUwEndvGmA36x\nKcdpwTqZmbpQbBY/PPsng+NXAy4jGJUdeGYQ23CkSi+k0ywjW7Cpai/BAoGAUC7y\nV632xjDAkqhDH6EVlKN/8ALFCose6KLAE7k6bylJboP5crtGfsvAfzHGmab2tXA3\nFiqjymdtPVl+QQPsCYEUAvoZlNFim2FQZEwyrDaG0A8DU0+eI8p8cxl84o6grAeV\njhwttS3J/l2WehN+zBWjQUW+tQQSJ9AEoWGHar0CgYBkKJiDbgoRavplF0z5HmpG\nkbZu1gR5FfbxdNpdwFThQWXhyurjmIQzBdQh6JIh8NrWuI3CPS+dY6MGHLyLs7+g\nXAabbs3mgj7noky9T5rdlG6HdBBz5laR4KplCjnA0EhRzqH9IubVCU92t3c6Q0lZ\nf6zzKVpofz5abnrqI5OZfA==\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-fbsvc@evriman-eaba7.iam.gserviceaccount.com",
+  client_id: "115341079689466900422",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40evriman-eaba7.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+}
+
+
+
+  
+  
